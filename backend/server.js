02b8ee918a32ba8f7f5f404209aadee857880761
Jorge Jiménez <jorge.jimenezmunoz.93@gmail.com>
2023-09-28 10:04:59 +0200

    install new dependencies and change backend server.js configuration

--- backend/server.js
+++ backend/server.js
@@ -1,61 +1,40 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
-const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+const OpenAI = require("openai");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/process-image", (req, res) => {
-  const { imageData } = req.body;
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-  console.log("Solicitud de procesamiento de imagen recibida");
+app.post("/process-text", async (req, res) => {
+  const { text } = req.body;
 
-  openai
-    .createCompletion({
-      model: "text-davinci-003",
-      prompt,
-    })
-    .then((response) => {
-      res.send(response);
-      console.log(response.data);
-    })
-    .catch((error) => {
-      res.send(error);
+  try {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
     });
 
-  // try {
-  //   const response = axios.post(
-  //     "https://api.openai.com/v1/vision/compute",
-  //     {
-  //       model: "data:image/*;base64," + imageData,
-  //     },
-  //     {
-  //       headers: {
-  //         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-  //       },
-  //     }
-  //   );
-  //   console.log("ok");
-  //   res.send("ok");
-  //   // console.log("Respuesta de OpenAI:", response.data);
-  // } catch (error) {
-  //   console.log("ko");
-  //   res.send("ko", response.data.error);
-  //   if (req.response) {
-  //     console.error("Respuesta de error de OpenAI:", error.response.data);
-  //   } else if (error.request) {
-  //     console.error("No hay respuesta de OpenAI:", error.request);
-  //   } else {
-  //     console.error("Error al procesar la imagen con OpenAI:", error.message);
-  //   }
-  //   res.status(500).json({ error: "Error al procesar la imagen con OpenAI" });
-  // }
+    const response = await openai.completions.create({
+      model: "gpt-3.5-turbo-instruct",
+      prompt: text,
+      max_tokens: 4097 - text.length,
+    });
+
+    console.log("Respuesta de OpenAI:", response);
+    res.json(response.data);
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      console.error(error.status);
+      console.error(error.message);
+      console.error(error.code);
+      console.error(error.type);
+    } else {
+      console.log(error);
+    }
+    res.send(error);
+  }
 });
 
 const PORT = process.env.PORT || 5000;
